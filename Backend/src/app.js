ae217edd099d92572b957b9483284de59f8d36aa
madhaviporte <madhaviporte2004@gmail.com>
2026-05-17 15:09:34 +0530

    add production cors origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,7 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin:[ "http://localhost:5173",
+    "https://snitch-gamma.vercel.app/"],
     methods: [ "GET", "POST", "PUT", "DELETE" ],
     credentials: true
 }))
