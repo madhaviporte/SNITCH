54245def29adcdd5f935ef5d0480176932c1824b
madhaviporte <madhaviporte2004@gmail.com>
2026-05-17 18:28:03 +0530

    fix cors origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
     origin:[ "http://localhost:5173",
-    "https://snitch-gamma.vercel.app/"],
+    "https://snitch-gamma.vercel.app"],
     methods: [ "GET", "POST", "PUT", "DELETE" ],
     credentials: true
 }))
