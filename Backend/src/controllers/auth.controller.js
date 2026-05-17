eb912b9a8d1b67deacf5d97ec2699ee86e493e4d
madhaviporte <madhaviporte2004@gmail.com>
2026-05-17 18:22:35 +0530

    complete

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -104,7 +104,7 @@ export const googleCallback = async (req, res) => {
 
     res.cookie("token", token)
 
-    res.redirect("http://localhost:5173/")
+    res.redirect("https://snitch-gamma.vercel.app/")
 }
 
 export const getMe = async (req, res) => {
