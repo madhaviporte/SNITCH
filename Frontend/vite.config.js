63a79b598258b72cad90c37b4714b9af925e0057
madhaviporte <madhaviporte2004@gmail.com>
2026-05-18 15:03:42 +0530

    chnages

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://snitch-pj8c.onrender.com/",
         changeOrigin: true,
         secure: false,
       }
